33950d177de394489e2d29acdcfc03ddb41e6820
Gery082 <geryfigueroa@gmail.com>
2018-11-19 16:31:34 -0500

    alertify

--- DattingAppSPA/src/app/app.module.ts
+++ DattingAppSPA/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
    declarations: [
@@ -24,7 +25,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    ],
    providers: [
        AuthService,
-       ErrorInterceptorProvider
+       ErrorInterceptorProvider,
+       AlertifyService
    ],
    bootstrap: [
       AppComponent
